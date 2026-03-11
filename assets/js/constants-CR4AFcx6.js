fb8f2d86a0a176ee4b786407cb5fc9bf470572da
kc502 <145409949+kc502@users.noreply.github.com>
2026-03-11 17:20:11 +0630

    Add files via upload

---
+++ assets/js/constants-CR4AFcx6.js
@@ -0,0 +1,51 @@
+const e = {
+    messages: ["I love you so much! ❤️", "Our Anniverasry", "I love you 💖", "25/08/2004", "Thank you for being my sunshine ", "Thank you for being my everything 💕", "You are my universe ", "There is no other", "You're amazing", "You make my heart smile ", "Love ya! 💖", "Honey bunch, you are my everything! "],
+    icons: ["♥", "💖", "❤️", "❤️", "💕", "💕"],
+    colors: "#ff6b9d",
+    images: ["https://cdn.deargift.online/uploads/z6654939494311_5eacc5c2b6fdbcbee6dcd20673a8040c.jpg"],
+    song: "eyenoselip.mp3",
+    isHeart: !0,
+    textHeart: "Trí Toán",
+    isSave: !0,
+    createdAt: "2025-05-30T00:00:00.000Z"
+}
+  , o = {
+    SMALL_MOBILE: 480,
+    MOBILE: 768
+}
+  , a = {
+    maxParticles: {
+        smallMobile: 80,
+        mobile: 120,
+        desktop: 300
+    },
+    particleInterval: {
+        mobile: 200,
+        desktop: 120
+    },
+    starCount: {
+        smallMobile: 250,
+        mobile: 350,
+        desktop: 500
+    }
+}
+  , s = {
+    particles: {
+        length: {
+            mobile: 5e3,
+            desktop: 1e4
+        },
+        duration: 4,
+        velocity: {
+            mobile: 50,
+            desktop: 80
+        },
+        effect: -1.3,
+        size: {
+            mobile: 6,
+            desktop: 8
+        }
+    }
+}
+  , i = "https://api.deargift.online/api";
+export {i as A, o as B, e as D, s as H, a};
